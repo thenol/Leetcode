1936b19a95e70a6e38006b4c388e71a4f6387625
thenol <277434255@qq.com>
2024-12-23 20:24:50 +0800

    归约态写法

--- Dynamic Programming/knapsack/01背包/474. 一和零.py
+++ Dynamic Programming/knapsack/01背包/474. 一和零.py
@@ -38,6 +38,37 @@ https://leetcode.cn/problems/ones-and-zeroes/description/?source=vscode
 from collections import Counter
 from functools import cache
 class Solution:
+    # 哨兵护法写法
+    # 最推荐写法，比较有意义——归约态从范围变成了单点
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        # preprocess
+        strs_cnt = []
+        for str in strs:
+            cnt = Counter(str)
+            strs_cnt.append((cnt['0'],cnt['1']))
+        
+        # 0/1 knapsack problem
+        # state: d[i][m][n] 表示前i个元素，剩余容量为m和n的最大子集长度
+        # 0<=i<len(strs)
+
+        # initialization
+        ...
+
+        # transition
+        @cache
+        def f(i, m, n):
+            """表示范围strs_cnt[:i]，剩余容量为m和n的最大子集长度"""
+            nonlocal strs_cnt
+            if i == 0: return 0 # 空数组，没元素可选了，返回0
+            zero_cnt, one_cnt = strs_cnt[i-1]
+            if 0 <= m-zero_cnt and 0<=n-one_cnt:
+                # 放得下，可放可不放
+                return max(f(i-1, m-zero_cnt, n-one_cnt) + 1, f(i-1, m, n))
+            else: # 放不下
+                return f(i-1, m, n)
+        return f(len(strs), m, n)
+
+    # 
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
         # preprocess
         strs_cnt = []
@@ -57,8 +88,8 @@ class Solution:
         def f(i, m, n):
             """表示前i个元素，剩余容量为m和n的最大子集长度"""
             nonlocal strs_cnt
-            if m < 0 or n < 0 or i < 0: # 边界处理
-                return 0 # 装不下
+            if i < 0: # 没元素可选了，返回0
+                return 0 
             zero_cnt, one_cnt = strs_cnt[i]
             if 0 <= m-zero_cnt and 0<=n-one_cnt:
                 # 放得下，可放可不放
@@ -66,3 +97,33 @@ class Solution:
             else: # 放不下
                 return f(i-1, m, n)
         return f(len(strs)-1, m, n)
+    
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        zero_one_list = []
+        for s in strs:
+            cnt = Counter(s)
+            zero_one_list.append((cnt['0'], cnt['1']))
+        
+        N = len(zero_one_list)
+
+        @cache
+        def f(i, j, k):
+            """到strs[i]，最多j个0，k个1"""
+            nonlocal zero_one_list
+
+            # 归约态
+            if i==0:
+                if zero_one_list[0][0]<=j and zero_one_list[0][1]<=k: return 1
+                else: return 0
+
+            ans = 0
+            # 不装i
+            if 0 <= i-1:
+                ans = max(ans, f(i-1, j, k))
+            
+            zero, one = zero_one_list[i]
+            # 可以装i
+            if zero<=j and one <=k and 0<=i-1:
+                ans = max(ans, f(i-1, j-zero, k-one) + 1)
+            return ans
+        return f(N-1, m, n)
