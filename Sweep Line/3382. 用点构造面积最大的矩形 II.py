c379ba69d78515f7daa357ca3dbfe66e922e15de
whichb6 <dfnanhai@sina.com>
2024-12-23 22:56:59 +0800

    sweep line

--- Sweep Line/3382. 用点构造面积最大的矩形 II.py
+++ Sweep Line/3382. 用点构造面积最大的矩形 II.py
@@ -61,3 +61,122 @@ https://leetcode.cn/problems/maximum-area-rectangle-with-point-constraints-ii/de
 """
 
 # 核心思路：扫描线
+
+"""
+更多题目：https://www.bilibili.com/video/BV16x4y1a7Ro/?spm_id_from=333.999.0.0&vd_source=b58f1d2059dc6db7819eeb654fe318be
+"""
+
+# 树状数组模板
+from collections import defaultdict
+from itertools import pairwise
+from bisect import bisect_left
+class Fenwick:
+    # 树状数组（Fenwick Tree）的构造函数，用于快速查询区间和
+    def __init__(self, n: int):
+        self.tree = [0] * (n + 1)  # 初始化树状数组
+
+    # 向树状数组中添加值，用于更新区间和
+    def add(self, i: int) -> None:
+        while i < len(self.tree):  # 循环直到i超出数组范围
+            self.tree[i] += 1  # 在树状数组的第i个位置加1
+            i += i & -i  # 移动到下一个需要更新的位置
+
+    # 查询[1, i]区间内的元素和
+    def pre(self, i: int) -> int:
+        res = 0  # 初始化结果变量
+        while i > 0:  # 循环直到i减到0
+            res += self.tree[i]  # 累加当前位置的值
+            i &= i - 1  # 移动到下一个需要累加的位置
+        return res
+
+    # 查询[l, r]区间内的元素和
+    def query(self, l: int, r: int) -> int:
+        return self.pre(r) - self.pre(l - 1)  # 利用前缀和的性质计算区间和
+
+# 离线版本的二维数点问题
+# 计算 [x1,x2] × [y1,y2] 这个矩形区域内的点的个数
+# 本题是只有询问，没有更新的二维数点问题
+# 刚才说的矩形，我们只保证了边界上没有其他点，但是区域内的点的个数还不知道
+# 给你一堆询问 queries[i] = [x1,x2,y1,y2] -> 区域内的点个数
+# 离线算法
+# 每个询问拆成两个（前缀和的思想）
+# 计算 x <= x1-1 和 y ∈ [y1,y2] 中的点的个数
+# 计算 x <= x2 和 y ∈ [y1,y2] 中的点的个数
+# 按照 x 去把这 2q 个拆开后的询问分组
+# 从小到大遍历 x，一边更新树状数组，一边回答询问
+
+class Solution:
+    def maxRectangleArea(self, xCoord: List[int], yCoord: List[int]) -> int:
+        # 使用字典存储每个x坐标对应的y坐标列表
+        x_map = defaultdict(list)  
+        # 使用字典存储每个y坐标对应的x坐标列表
+        y_map = defaultdict(list)  
+        for x, y in zip(xCoord, yCoord):
+            x_map[x].append(y)  # 将点加入x_map
+            y_map[y].append(x)  # 将点加入y_map
+
+        # 预处理每个点正下方的点
+        below = {}
+        for x, ys in x_map.items():
+            ys.sort()  # 对每个x坐标的y坐标列表进行排序
+            for y1, y2 in pairwise(ys):  # 遍历排序后的y坐标列表
+                below[(x, y2)] = y1  # 存储正下方的点，y1<=y2
+
+        # 预处理每个点正左边的点
+        left = {}
+        for y, xs in y_map.items():
+            xs.sort()  # 对每个y坐标的x坐标列表进行排序
+            for x1, x2 in pairwise(xs):  # 遍历排序后的x坐标列表
+                left[(x2, y)] = x1  # 存储正左边的点，x1<=x2
+
+        # 离散化处理：将具体数值转换成下标，降低遍历的长度，从而降低复杂度
+        xs = sorted(x_map)  # 对x坐标进行排序
+        ys = sorted(y_map)  # 对y坐标进行排序
+
+        # 收集询问：矩形区域（包括边界）的点的个数
+        queries = []
+        # 枚举 (x2,y2) 作为矩形的右上角
+        for x2, list_y in x_map.items(): # 遍历横坐标，以及这个垂线上面的所有纵坐标，即扫描线开始
+            for y1, y2 in pairwise(list_y): # 遍历每个相邻的纵坐标
+                # 计算矩形左下角 (x1,y1)
+                x1 = left.get((x2, y2), None) # 得到x2左边相邻坐标x1
+                # 确保矩形的左下角和右下角的左边点的横坐标是x1
+                # 确保矩形的左上角和右下角的下边点的纵坐标是y1
+                if x1 is not None and left.get((x2, y1), None) == x1 and below.get((x1, y2), None) == y1: # 这波验证能够确认矩形边界只有4个点，由于是遍历相邻点，从而已经是最小矩形了；❗️注意区域内的点的个数不清楚，需要通过后续树状数组来求，这也是为什么要构建询问的原因
+                    queries.append(( # 记录这样一个横纵坐标区间及其面积；x1<=x2, y1<=y2
+                        bisect_left(xs, x1),  # 离散化x1，用下标替代，防止x1和x2，或者纵坐标之间距离太大，降低遍历复杂度；例如[10000, 100000000000]转换成下标表示为[0, 1]，而不用遍历range(10000, 100000000000)
+                        bisect_left(xs, x2), # 排序之后数组，搜索用二分查找加速
+                        bisect_left(ys, y1),
+                        bisect_left(ys, y2),
+                        (x2 - x1) * (y2 - y1),
+                    ))
+
+        # 离线询问；构建边界事件
+        grouped_queries = [[] for _ in range(len(xs))] # 按照离散化之后横坐标排序的顺序来记录事件序列，说白了就是离开右边界的时候就开始减左边界统计区间内点数
+        for i, (x1, x2, y1, y2, _) in enumerate(queries):
+            if x1 > 0:
+                # 因为要统计的是横坐标在[x1, x2]区间点的个数，划分边界 x1-1,[x1, x2]，右边界 - 左边界左邻居
+                grouped_queries[x1 - 1].append((i, -1, y1, y2)) # 左边界左侧，并且记录要处理的上下边界
+            grouped_queries[x2].append((i, 1, y1, y2)) # 右边界，并且记录要处理的上下边界
+
+        # 回答询问
+        res = [0] * len(queries)
+        tree = Fenwick(len(ys))
+        # ❗️从小到大遍历xs数组，并且开始处理和x相关的边界事件
+        for x, qs in zip(xs, grouped_queries): # 注意xs是排序之后的数组；❗️这里xs和边界事件已经绑定了，所以在遍历到x到时候，就直接可以处理x对应的边界事件了
+            # 从小到大遍历xs，逐渐把横坐标为 x 的所有点都加到树状数组中
+            for y in x_map[x]:
+                tree.add(bisect_left(ys, y) + 1)  # 离散化y；树状数组下标从1开始，记录横坐标x上所有纵坐标点
+            for qid, sign, y1, y2 in qs:
+                # query作用：查询横坐标 <= x（已满足）且纵坐标在 [y1,y2] 中的点的个数；
+                # 核心原理：
+                # 1. 假设和qid相关联的区域为[x1, x2]，即到达左边界左侧x1-1时，计算一下 {X<=x1-1, y1<=Y<=y2}区域点数的负数
+                # 2. 到达右边界x2时，计算一下 {X<=x2, y1<=Y<=y2}区域点数，然后相加，即为区域{x1<=X<=x2, y1<=Y<=y2}区域的点数
+                res[qid] += sign * tree.query(y1 + 1, y2 + 1) # 右边界 - 左边界左邻居 得到区域内点数
+
+
+        ans = -1
+        for cnt, q in zip(res, queries):
+            if cnt == 4:
+                ans = max(ans, q[4])  # q[4] 保存着矩形面积
+        return ans  # 返回最大矩形面积
